38289203dcbb11e77575f35bcabc7381ed9767ca
salosin <salosin@drom.ru>
2019-03-14 15:18:16 +0700

    add sign in activity and layout

--- app/src/main/java/com/github/salonkasoli/githubsearch/MainActivity.kt
+++ app/src/main/java/com/github/salonkasoli/githubsearch/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.github.salonkasoli.githubsearch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.github.salonkasoli.githubsearch.login.SignInActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.sign_in_button).setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/github/salonkasoli/githubsearch/login/SignInActivity.kt
@@ -0,0 +1,7 @@
+package com.github.salonkasoli.githubsearch.login
+
+import androidx.appcompat.app.AppCompatActivity
+
+class SignInActivity : AppCompatActivity() {
+
+}
